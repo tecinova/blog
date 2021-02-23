3fb5eebc496075a9b6fdec700dbf97f84cd1feab
Roberto Alves <77143340+RobertoAlves86@users.noreply.github.com>
2021-02-23 18:45:20 -0300

    Update services.js

--- src/constants/services.js
+++ src/constants/services.js
@@ -8,18 +8,18 @@ export default [
   {
     icon: <FaDraftingCompass />,
     title: "Design",
-    text: "•Pacotes de imagens para mídias sociais•Cartões de visita e digitais interativos•Desenvolvimento de logotipos.",
+    text: "Pacotes de imagens para mídias sociais,cartões de visita e digitais interativos,desenvolvimento de logotipos...",
   },
   {
     icon: <FaLaptopCode />,
     title: "Marketing Digital",
     text:
-      "•Digitalização de negócios e pessoas•E-mails personalizados•Ecommerce (comércio eletrônico)•Customização de ferramentas de gestão empresarial•Soluções em nuvem•Treinamentos e serviços pela web•Estratégias para mídias sociais•Eventos online e lives•Divulgação para vendas online.",
+      "Digitalização de negócios e personalidades,e-mails personalizados,ecommerce (comércio eletrônico),customização de ferramentas de gestão empresarial,soluções em nuvem,treinamentos e serviços pela web,estratégias para mídias sociais,eventos online e lives,divulgação para vendas online...",
   },
   {
     icon: <FaHardHat />,
     title: "Software",
-    text: "Suporte e treinamento in loco ou in company a usuários de computador quanto a sistemas, aplicativos e ferramentas corporativas e residenciais.",
+    text: "Suporte e treinamento in loco ou in company a usuários de computador quanto a sistemas, aplicativos e ferramentas sejam corporativas e ou residenciais.",
   },
   {
     icon: <FaFeatherAlt />,
