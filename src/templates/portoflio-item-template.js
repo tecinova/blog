f4a149b91b60eef2e0887f1e9dc706d7995a6686
Roberto Alves <77143340+RobertoAlves86@users.noreply.github.com>
2021-02-24 01:01:29 -0300

    Update portoflio-item-template.js

--- src/templates/portoflio-item-template.js
+++ src/templates/portoflio-item-template.js
@@ -42,7 +42,7 @@ const PortfolioItemTemplate = ({ data }) => {
             className="btn-primary"
             style={{ marginTop: "3rem" }}
           >
-            categoria {slug}
+            + itens da categoria {slug}
           </Link>
         </div>
       </PortfolioItemWrapper>
