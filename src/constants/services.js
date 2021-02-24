1ef56f8e36e5d0927d88f8794bde0d7c7ab18988
Roberto Alves <77143340+RobertoAlves86@users.noreply.github.com>
2021-02-23 23:27:00 -0300

    Update services.js

--- src/constants/services.js
+++ src/constants/services.js
@@ -4,6 +4,7 @@ import { FaLaptopCode } from "react-icons/fa"
 import { FaHardHat } from "react-icons/fa"
 import { FaFeatherAlt } from "react-icons/fa"
 import { FaPhotoVideo } from "react-icons/fa"
+import { FaBullhorn } from "react-icons/fa"
 
 export default [
   {
@@ -12,13 +13,13 @@ export default [
     text: "Pacotes de imagens para mídias sociais,cartões de visita e digitais interativos,desenvolvimento de logotipos...",
   },
   {
-    icon: <FaLaptopCode />,
+    icon: <FaBullhorn />,
     title: "Marketing Digital",
     text:
       "Digitalização de negócios e personalidades,e-mails personalizados,ecommerce (comércio eletrônico),customização de ferramentas de gestão empresarial,soluções em nuvem,treinamentos e serviços pela web,estratégias para mídias sociais,eventos online e lives,divulgação para vendas online...",
   },
   {
-    icon: <FaHardHat />,
+    icon: <FaLaptopCode />,
     title: "Software",
     text: "Suporte e treinamento in loco ou in company a usuários de computador quanto a sistemas, aplicativos e ferramentas sejam corporativas e ou residenciais.",
   },
