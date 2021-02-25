f1a3e87ab83c6e1875b7ea52b16c9fa6ee925e61
Roberto Alves <77143340+RobertoAlves86@users.noreply.github.com>
2021-02-25 01:41:07 -0300

    Update services.js

--- src/constants/services.js
+++ src/constants/services.js
@@ -9,15 +9,15 @@ import { FaTools } from "react-icons/fa"
 
 export default [
   {
-    icon: <FaPhotoVideo />,
-    title: "Design",
-    text: "Cardápio,cartão digital interativo,pacotes de imagens e vídeos para mídias sociais,identidade corporativa da qual fazem parte dentre outras coisas,logotipo e cartões de visita...",
-  },
-  {
     icon: <FaMobile />,
     title: "Transformação Digital",
     text:
       "Planejamento para presença digital e inserção,digitalização de pequenos negócios,desenvolvimento de sistemas para web e mídias digitais de comunicação corporativa,planejamento e preparação para ecommerce,mídias sociais e anúncios*,chatbot e automatizações,home office,eventos online e lives...",
+  },
+    {
+    icon: <FaPhotoVideo />,
+    title: "Design",
+    text: "Cardápio,cartão digital interativo,pacotes de imagens e vídeos para mídias sociais,identidade corporativa da qual fazem parte dentre outras coisas,logotipo e cartões de visita...",
   },
   {
     icon: <FaLaptopCode />,
